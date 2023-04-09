ca35ce1fd4451ed22c11afbd140a25192e3d7a8a
GBarbosa1 <barbosa.gbsantos@gmail.com>
2023-04-08 21:44:15 -0300

    Performance_enchacement_on_engine

---
+++ scrap_engine.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+
+def scrap_init(url):
+    try:
+        chrome_options = Options()
+        chrome_options.add_experimental_option("detach", True)
+        user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+        chrome_options.add_argument(f'user-agent={user_agent}')
+        browser = webdriver.Chrome(executable_path='chromedriver.exe', chrome_options=chrome_options)
+        browser.get(url)
+        
+    except:
+        soup = ''
+        
+    return browser
+    
+def get_url(browser,url):
+    browser.get(url)
+
+def get_element_xpath(browser,xpath):
+    try:
+        element = browser.find_element(By.XPATH, xpath)
+        return element
+
+    except:
+        print("Unable to fetch")
+        pass
+    
+    
+
+def click(element):
+    element.click()
+
+def send_keys(element, text):
+    element.send_keys(text)
+
+def buffer(seconds):
+    time.sleep(seconds)
+
+def strip(element):
+    try:
+        num = element.text
+        return num
+
+    except:
+        num = 'null'
+        return num
