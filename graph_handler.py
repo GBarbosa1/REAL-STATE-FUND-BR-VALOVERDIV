0020e147bf4095a7e0246d2aebc602da9f800df0
GBarbosa1 <barbosa.gbsantos@gmail.com>
2023-04-29 12:20:50 -0300

    Removed seaborn and added a hover event in plot

--- graph_handler.py
+++ graph_handler.py
@@ -1,14 +1,54 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
-def plotter(xdata, ydata, title, x_label, y_label, xlim_min, xlim_max, ylim_min, ylim_max, hue):
-    fig = plt.figure(figsize=(100,100), dpi = 200)
-    sns.set(font_scale = 0.7)
-    sns.color_palette("rocket_r", as_cmap=True)
-    plot = sns.scatterplot(x=xdata, y=ydata, hue = hue)
-    plot.set_title(title, weight = 'bold')
-    plot.set_xlabel(x_label, weight = 'bold')
-    plot.set_ylabel(y_label, weight = 'bold')
-    plot.grid(True)
+def plotter(xdata, ydata):
+    colors = np.random.randint(1, 5, size=len(xdata))
+    norm = plt.Normalize(1, 4)
+    cmap = plt.cm.PiYG
+    fig, ax = plt.subplots()
+    scatter = plt.scatter(
+        x=xdata,
+        y=ydata,
+        s=100)
+    annotation = ax.annotate(
+    text='',
+    xy=(0, 0),
+    xytext=(15, 15),
+    textcoords='offset points',
+    bbox={'boxstyle': 'round', 'fc': 'w'},
+    arrowprops={'arrowstyle': '->'}
+    )
+    annotation.set_visible(False)
+    def motion_hover(event):
+        annotation_visbility = annotation.get_visible()
+        if event.inaxes == ax:
+            is_contained, annotation_index = scatter.contains(event)
+            if is_contained:
+                data_point_location = scatter.get_offsets()[annotation_index['ind'][0]]
+                annotation.xy = data_point_location
+
+                text_label = '({0:.2f}, {0:.2f})'.format(data_point_location[0], data_point_location[1])
+                annotation.set_text(text_label)
+
+                annotation.get_bbox_patch().set_facecolor(cmap(norm(colors[annotation_index['ind'][0]])))
+                annotation.set_alpha(0.4)
+
+                annotation.set_visible(True)
+                fig.canvas.draw_idle()
+            else:
+                if annotation_visbility:
+                    annotation.set_visible(False)
+                    fig.canvas.draw_idle()
+    fig.canvas.mpl_connect('motion_notify_event', motion_hover)
     plt.show()
-    return 
+    return
+
+asserted_fii_list = pd.read_csv('FII_LIST_ACTIVE.CSV',sep=',')
+
+plotter(asserted_fii_list.pop('DIV'), asserted_fii_list.pop('PVP'))
+
+
+
+
