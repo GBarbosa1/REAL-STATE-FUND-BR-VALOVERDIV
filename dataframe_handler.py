194c04e8b8a19e9294f5c26cf18c72a05d4a4886
GBarbosa1 <barbosa.gbsantos@gmail.com>
2023-05-07 01:14:15 -0300

    Added a truncate function into dataframe_handler

--- dataframe_handler.py
+++ dataframe_handler.py
@@ -7,3 +7,16 @@ def dataframe_build(data, columns):
 def dataframe_sort(dataframe, by, ascending, inplace):
     dataframe.sort_values(by = by, ascending = ascending, inplace = inplace)
     return dataframe
+
+def dataframe_astype(dataframe,column,type):
+    dataframe[column] = dataframe[column].astype(type)
+    return dataframe
+
+def dataframe_str_trunc(dataframe, column,number,direction):
+    if direction=='right':
+        dataframe[column] = dataframe[column].str[:-number]
+    elif direction=='left':
+        dataframe[column] = dataframe[column].str[number:]
+    else:
+        pass
+    return dataframe
